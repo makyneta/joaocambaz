aa1972c6c8aa12fce171f306a613a74745a95f49
Tomás Mota <208397927+tomasmotahq@users.noreply.github.com>
2025-11-26 09:02:23 +0000

    deploy

--- a/jc/js/script.js
+++ a/jc/js/script.js
@@ -8,7 +8,7 @@ document.querySelectorAll('.year').forEach(el => {
 fetch("/a/jc/h/header-jc.html")
   .then(response => response.text())
   .then(data => {
-    document.getElementById("header").innerHTML = data;
+    document.getElementById("header-jc").innerHTML = data;
 
     const script = document.createElement("script");
     script.src = "/a/jc/h/script.js";
